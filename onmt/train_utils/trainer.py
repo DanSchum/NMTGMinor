8478bb334f1cfc5b006c561a380b28f85027f8ba
DanSchum <daniel.schumacher93@gmail.com>
2018-10-17 13:57:28 +0200

    Removed catch, because if perplexity goes to infinity something is wrong in the training.

--- onmt/train_utils/trainer.py
+++ onmt/train_utils/trainer.py
@@ -264,26 +264,16 @@ class XETrainer(BaseTrainer):
                 
                 
                 if i == 0 or (i % opt.log_interval == -1 % opt.log_interval):
-                    try:
-                        print(("Epoch %2d, %5d/%5d; ; ppl: %6.2f ; lr: %.7f ; num updates: %7d " +
-                            "%5.0f src tok/s; %5.0f tgt tok/s; %s elapsed") %
-                            (epoch, i+1, len(trainData),
-                            math.exp(report_loss / report_tgt_words),
-                            optim.getLearningRate(),
-                            optim._step,
-                            report_src_words/(time.time()-start),
-                            report_tgt_words/(time.time()-start),
-                            str(datetime.timedelta(seconds=int(time.time() - self.start_time)))))
-                    except OverflowError:
-                        print(("Epoch %2d, %5d/%5d; ; ppl: %6.2f ; lr: %.7f ; num updates: %7d " +
-                               "%5.0f src tok/s; %5.0f tgt tok/s; %s elapsed") %
-                              (epoch, i + 1, len(trainData),
-                               float('inf'),
-                               optim.getLearningRate(),
-                               optim._step,
-                               report_src_words / (time.time() - start),
-                               report_tgt_words / (time.time() - start),
-                               str(datetime.timedelta(seconds=int(time.time() - self.start_time)))))
+
+                    print(("Epoch %2d, %5d/%5d; ; ppl: %6.2f ; lr: %.7f ; num updates: %7d " +
+                        "%5.0f src tok/s; %5.0f tgt tok/s; %s elapsed") %
+                        (epoch, i+1, len(trainData),
+                        math.exp(report_loss / report_tgt_words),
+                        optim.getLearningRate(),
+                        optim._step,
+                        report_src_words/(time.time()-start),
+                        report_tgt_words/(time.time()-start),
+                        str(datetime.timedelta(seconds=int(time.time() - self.start_time)))))
 
                     report_loss, report_tgt_words = 0, 0
                     report_src_words = 0
