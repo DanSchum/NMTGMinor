ae8e335862d7b6f51d1d56aaf303910005fff49e
DanSchum <daniel.schumacher93@gmail.com>
2018-10-22 10:14:05 +0200

    Bug fix in start script

--- eval_command_wrapper.sh
+++ eval_command_wrapper.sh
@@ -19,5 +19,5 @@ echo $1
 #Parameter of script is model
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 translate.py \
 -model $1 \
--src /home/dschumacher/dschumacher_working_dir/evaluation/evaluation_data/tedTalks/preproc_bpe_short/encoded_validationSplitSource_lowercase_tokenizer_cleaned_short.txt
+-src /home/dschumacher/dschumacher_working_dir/evaluation/evaluation_data/tedTalks/preproc_bpe_short/encoded_validationSplitSource_lowercase_tokenizer_cleaned_short.txt \
 -output $outputDirectory"pred.txt"
