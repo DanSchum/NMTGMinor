8b3b1ced14983397a9f3ff242a7345ef9cdd74f2
DanSchum <daniel.schumacher93@gmail.com>
2018-10-22 11:39:38 +0200

    Added parameters to train start script

--- train_command_wrapper.sh
+++ train_command_wrapper.sh
@@ -3,4 +3,6 @@
 -data /home/dschumacher/dschumacher_working_dir/training_data/ted_talks_english/after_preprocessing/preprocessing_19_10_18_bpe_complete/preproc_output \
 -data_format bin \
 -model transformer \
--batch_size_words 4096
+-batch_size_words 4096 \
+-batch_size_sents 64 \
+-batch_size_update 4096
