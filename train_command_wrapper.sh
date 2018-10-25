deb9353c13c5a7da85955c2e3267760c4854fc5f
Daniel Schumacher <dschumacher@rg3hpc1.ilkd.uni-karlsruhe.de>
2018-10-25 12:09:04 +0200

    changed start scripts.

--- train_command_wrapper.sh
+++ train_command_wrapper.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 train.py \
--data /home/dschumacher/dschumacher_working_dir/training_data/ted_talks_english/after_preprocessing/preprocessing_19_10_18_bpe_complete/preproc_output \
+-data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/tedtalks/transformer_preproc_truncated_source/preproc_output \
 -data_format bin \
--model transformer \
--batch_size_words 4096 \
--batch_size_sents 64 \
--batch_size_update 4096
+-model transformer 
+#-batch_size_words 4096 \
+#-batch_size_sents 64 \
+#-batch_size_update 4096
