b1b0008f48a0b582930c4d7f4579110b9e4c9d7c
Daniel Schumacher <dschumacher@rg3hpc1.ilkd.uni-karlsruhe.de>
2018-11-07 11:51:20 +0100

    Updated start scripts

--- preprocessing_command_wrapper.sh
+++ preprocessing_command_wrapper.sh
@@ -5,7 +5,7 @@
 -train_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/bpe_25_10_18/encoded_train.title.txt \
 -valid_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/bpe_25_10_18/encoded_valid.article.filter.txt \
 -valid_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/bpe_25_10_18/encoded_valid.title.filter.txt \
--save_data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/bpe_25_10_18/preproc_output \
+-save_data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/transformer_preproc_bpe_06_11_18/preproc_output \
 -format raw \
--src_seq_length 1024 \
--tgt_seq_length 512
+-src_seq_length 333 \
+-tgt_seq_length 333
--- train_command_wrapper.sh
+++ train_command_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 train.py \
--data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/tedtalks/transformer_preproc_truncated_source/preproc_output \
+-data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/transformer_preproc_bpe_06_11_18/preproc_output \
 -data_format raw \
 -model transformer \
 -learning_rate 0.001 \
