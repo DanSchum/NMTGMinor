f256f6ffce33abd9a06409bfde4af867320e5375
DanSchum <daniel.schumacher93@gmail.com>
2018-11-07 11:22:35 +0100

    Last preprocess was raw format.

--- train_command_wrapper.sh
+++ train_command_wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 train.py \
 -data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/tedtalks/transformer_preproc_truncated_source/preproc_output \
--data_format bin \
+-data_format raw \
 -model transformer \
 -learning_rate 0.001 \
 -log_interval 1
