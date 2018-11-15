0f546e37625a898d62cb0c911bbe814349cd8f11
DanSchum <daniel.schumacher93@gmail.com>
2018-11-15 13:10:29 +0100

    update run scripts

--- cmd_preprocessing_command_wrapper.sh
+++ cmd_preprocessing_command_wrapper.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 preprocess.py \
--train_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/preproc_cleanLowTokenize_2018_11_08_17_27_41/splits/training_source_preproc_source_summaries.txt \
--train_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/preproc_cleanLowTokenize_2018_11_08_17_27_41/splits/training_target_preproc_target_titles.txt \
--valid_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/preproc_cleanLowTokenize_2018_11_08_17_27_41/splits/validation_source_preproc_source_summaries.txt \
--valid_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/preproc_cleanLowTokenize_2018_11_08_17_27_41/splits/validation_target_preproc_target_titles.txt \
--save_data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/transformer_preproc_no_bpe_2018_11_15  \
+-train_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/clean_low_tokenization_22_10_18/preproc_train.article.txt \
+-train_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/clean_low_tokenization_22_10_18/preproc_train.title.txt \
+-valid_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/clean_low_tokenization_22_10_18/preproc_valid.article.filter.txt \
+-valid_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/clean_low_tokenization_22_10_18/preproc_valid.title.filter.txt \
+-save_data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/transformer_preproc_no_bpe_2018_11_15  \
 -format raw \
 -src_seq_length 512 \
 -tgt_seq_length 256
