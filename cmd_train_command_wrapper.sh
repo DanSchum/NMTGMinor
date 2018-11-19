66a16eb17ee2815f66bd5a7f6c8c75e6dd490a96
DanSchum <daniel.schumacher93@gmail.com>
2018-11-19 13:42:33 +0100

    update run scripts

--- cmd_train_command_wrapper.sh
+++ cmd_train_command_wrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 train.py \
--data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/transformer_preproc_no_bpe_2018_11_15/transformer_preproc_no_bpe_2018_11_15.train.pt \
+-data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/transformer_preproc_2018_11_11/transformer_preproc_2018_11_11.train.pt \
 -data_format raw \
 -model transformer \
 -learning_rate 0.001 \
 -layers 6 \
--log_interval 10 \
--save_every 3000 \
+-log_interval 100 \
+-save_every 1400 \
 -save_model $1
 #-batch_size_words 4096 \
 #-batch_size_sents 64 \
