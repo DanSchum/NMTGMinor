81e8a11344b5437fe2c238ca28d709a50c8b45ae
DanSchum <daniel.schumacher93@gmail.com>
2018-11-20 15:14:15 +0100

    update evaluation run scripts. Now having two parameters (model and outputPath)

--- cmd_eval_command_wrapper.sh
+++ cmd_eval_command_wrapper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Parameter of Script
+# $1: (First Parameter) Model which should be used for translation/ evaluation
+# $2: (Second Parameter) Output Path, where the output should be stored. (!!!No ending / at path!!!)
 
 timestamp() {
   date +"%Y_%m_%d_%H_%M_%S"
@@ -7,7 +10,7 @@ timestamp() {
 
 # start training
 timestampValue=$(timestamp)
-outputDirectory="/home/dschumacher/dschumacher_working_dir/evaluation/evaluation_output/eval_"$timestampValue"/"
+outputDirectory=$2"/eval_"$timestampValue"/"
 
 
 if [ ! -d "$outputDirectory" ]; then
@@ -17,7 +20,11 @@ fi
 echo $1
 
 #Parameter of script is model
+#Hyperparameters are set like mentioned in Wiki Article Paper: https://arxiv.org/pdf/1801.10198.pdf
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 translate.py \
 -model $1 \
 -src /home/dschumacher/dschumacher_working_dir/evaluation/evaluation_data/tedTalks/preproc_bpe_short/encoded_validationSplitSource_lowercase_tokenizer_cleaned_short.txt \
--output $outputDirectory"pred.txt"
+-output $outputDirectory"pred.txt" \
+-beam_size 4 \
+-alpha 0.6 #Length Penalty coefficient (larger alpha results in longer translations)
+
--- start_evaluation.sh
+++ start_evaluation.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+#Parameter of Script
+# $1: (First Parameter) Model which should be used for translation/ evaluation
+# $2: (Second Parameter) Output Path, where the output should be stored. (!!!No ending / at path!!!)
+
+
 # Define a timestamp function
 timestamp() {
   date +"%Y_%m_%d_%H_%M_%S"
@@ -9,4 +15,4 @@ timestamp() {
 timestampValue=$(timestamp)
 outputFilename="logs/outputEvaluation_"$timestampValue".log"
 #echo $outputFilename
-sbatch -c 1 --mem=10000 -t 1-00 -p lowGPU -w i13hpc51 -o $outputFilename -e $outputFilename cmd_eval_command_wrapper.sh $1
+sbatch -c 1 --mem=10000 -t 1-00 -p lowGPU -w i13hpc51 -o $outputFilename -e $outputFilename cmd_eval_command_wrapper.sh $1 $2
