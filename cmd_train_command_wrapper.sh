b9087d89dd6729ba411788b5d47f4c99215af146
DanSchum <daniel.schumacher93@gmail.com>
2018-11-22 21:40:46 +0100

    uodate preprocessing run script.

--- cmd_train_command_wrapper.sh
+++ cmd_train_command_wrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 train.py \
--data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/transformer_preproc_2018_11_11/transformer_preproc_2018_11_11.train.pt \
+-data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/transformer_moses_subword_nmt_2018_11_22/transformer_moses_subword_nmt_2018_11_22.train.pt \
 -data_format raw \
 -model transformer \
 -learning_rate 0.001 \
 -layers 6 \
 -log_interval 100 \
--save_every 1400 \
+-save_every 3000 \
 -save_model $1
 #-batch_size_words 4096 \
 #-batch_size_sents 64 \
--- start_training.sh
+++ start_training.sh
@@ -8,7 +8,7 @@ timestamp() {
 # start training
 timestampValue=$(timestamp)
 outputFilename="logs/outputTraining_"$timestampValue".log"
-mkdir "/home/dschumacher/dschumacher_working_dir/models/papers_arxiv_own/category_computer_science/Training_output_"$timestampValue
-outputModel="/home/dschumacher/dschumacher_working_dir/models/papers_arxiv_own/category_computer_science/Training_output_"$timestampValue"/model_"
+mkdir "/home/dschumacher/dschumacher_working_dir/models/Gigaword/Training_output_"$timestampValue
+outputModel="/home/dschumacher/dschumacher_working_dir/models/Gigaword/Training_output_"$timestampValue"/model_"
 #echo $outputFilename
 sbatch -c 1 --mem=10000 -t 5-00 -p lowGPU -w i13hpc51 -o $outputFilename -e $outputFilename cmd_train_command_wrapper.sh $outputModel
