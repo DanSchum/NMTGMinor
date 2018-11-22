3c9265edd8f3e5eb68a54d41a9cb4116583562b7
DanSchum <daniel.schumacher93@gmail.com>
2018-11-22 21:18:46 +0100

    uodate preprocessing run script.

--- cmd_preprocessing_command_wrapper.sh
+++ cmd_preprocessing_command_wrapper.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 preprocess.py \
--train_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/train.article.txt \
--train_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/train.title.txt \
--valid_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/valid.article.filter.txt \
--valid_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/valid.title.filter.txt \
+-train_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_train.article.txt \
+-train_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_train.title.txt \
+-valid_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_valid.article.filter.txt \
+-valid_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_valid.title.filter.txt \
 -save_data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/transformer_moses_subword_nmt_2018_11_22  \
 -format raw \
 -src_seq_length 512 \
