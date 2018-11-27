c6ea1342f99e42bb7ff2869e97edf7e609f4c45e
DanSchum <daniel.schumacher93@gmail.com>
2018-11-27 18:38:31 +0100

    update training start script

--- cmd_train_command_wrapper.sh
+++ cmd_train_command_wrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-/home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 train.py \
--data /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/transformer_preproc_2018_11_26/transformer_moses_subword_nmt_2018_11_22.train.pt \
+/project/iwslt2014c/MT/user/dschumacher/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 train.py \
+-data /project/iwslt2014c/MT/user/dschumacher/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/transformer_preproc_2018_11_26/transformer_moses_subword_nmt_2018_11_22.train.pt \
 -data_format raw \
 -model transformer \
 -learning_rate 0.001 \
 -layers 6 \
--log_interval 100 \
--save_every 3000 \
+-log_interval 1 \
+-save_every 1 \
 -save_model $1
 #-batch_size_words 4096 \
 #-batch_size_sents 64 \
