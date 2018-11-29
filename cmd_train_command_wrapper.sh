95774f8bc8daaa8a86bbd71d4f60af9c4789acb6
Daniel Schumacher <dschumacher@rg3hpc1.ilkd.uni-karlsruhe.de>
2018-11-29 16:14:46 +0100

    Update of run scripts

--- cmd_train_command_wrapper.sh
+++ cmd_train_command_wrapper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-source /home/dschumacher/dschumacher_working_dir/anaconda/bin/activate /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6
+source /home/dschumacher/dschumacher_working_dir/anaconda/bin/activate /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6_cuda_80
 
 python3.6 train.py \
 -data /project/student_projects2/dschumacher/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/transformer_preproc_2018_11_29/transformer_moses_subword_nmt_2018_11_29.train.pt \
@@ -11,7 +11,7 @@ python3.6 train.py \
 -layers 6 \
 -log_interval 1 \
 -save_every 1 \
--gpus 1 \
+-gpus 0 \
 -save_model $1
 #-batch_size_words 4096 \
 #-batch_size_sents 64 \
