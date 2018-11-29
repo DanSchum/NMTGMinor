7b3ed91f6b041dc331cc26f692ef02738ccdf65a
DanSchum <daniel.schumacher93@gmail.com>
2018-11-29 17:23:13 +0100

    Run script with papers_arxiv_own Category Computer science parameters.

--- cmd_preprocessing_command_wrapper.sh
+++ cmd_preprocessing_command_wrapper.sh
@@ -9,6 +9,7 @@
 -valid_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/subword_mnt_2018_11_26/splits/validation_target_bpe_target_titles.txt \
 -save_data /project/student_projects2/dschumacher/preprocessing/after_preprocessing/papers_arxiv_own/category_computer_science/transformer_preproc_2018_11_29/transformer_moses_subword_nmt_2018_11_29  \
 -format raw \
+-sort_by_target \
 -src_seq_length 1024 \
 -tgt_seq_length 512
 
