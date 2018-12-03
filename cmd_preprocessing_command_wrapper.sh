6788149c64ec325ea5ca2ce0c976112cdc668502
DanSchum <daniel.schumacher93@gmail.com>
2018-12-03 14:16:23 +0100

    Added README output to preprocessing script

--- cmd_preprocessing_command_wrapper.sh
+++ cmd_preprocessing_command_wrapper.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
-#Dont forget / at end of save_data path!!!!!!!!!!!s
+outputPath="/project/student_projects2/dschumacher/preprocessing/after_preprocessing/Gigaword/transformer_2018_11_29/"
+nameOutputModel="transformer_moses_subword_nmt_2018_11_29"
+sourcePath="/home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/"
+
+trainSourceFilename="bpe_train.article.txt"
+trainTargetFilename="bpe_train.title.txt"
+validSourceFileName="bpe_valid.article.filter.txt"
+validTargetFileName="bpe_valid.title.filter.txt"
+
+src_seq_length=512
+tgt_seq_length=256
+
+logFile=$outputPath"README.txt"
+touch $logFile
+echo "Source Path: "$sourcePath >> $logFile
+echo "Target Path: "$outputPath >> $logFile
+echo "Source Sequence Length: "$src_seq_length>> $logFile
+echo "Target Sequence Length: "$tgt_seq_length >> $logFile
 
 /home/dschumacher/dschumacher_working_dir/anaconda/envs/NMTGMinor_env_python3_6/bin/python3.6 preprocess.py \
--train_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_train.article.txt \
--train_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_train.title.txt \
--valid_src /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_valid.article.filter.txt \
--valid_tgt /home/dschumacher/dschumacher_working_dir/preprocessing/after_preprocessing/Gigaword/subword_nmt_2018_11_22_8th/bpe_valid.title.filter.txt \
--save_data /project/student_projects2/dschumacher/preprocessing/after_preprocessing/Gigaword/transformer_2018_11_29/transformer_moses_subword_nmt_2018_11_29  \
+-train_src $sourcePath$trainSourceFilename \
+-train_tgt $sourcePath$trainTargetFilename \
+-valid_src $sourcePath$validSourceFileName \
+-valid_tgt $sourcePath$validTargetFileName \
+-save_data $outputPath$nameOutputModel \
 -format raw \
 -sort_by_target \
--src_seq_length 512 \
--tgt_seq_length 256
+-src_seq_length $src_seq_length \
+-tgt_seq_length $tgt_seq_length
 
 ./start_training.sh
