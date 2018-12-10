fb7d120916dff4c0b57ab5cb39ca5fc6de77fa44
DanSchum <daniel.schumacher93@gmail.com>
2018-12-10 16:42:50 +0100

    Added local mode to training

--- start_training.sh
+++ start_training.sh
@@ -27,7 +27,8 @@ outputFilename="logs/outputTraining_"$timestampValue".log"
 
 
 if [$3 == "local"]; then
-    sbatch -t 3-00 -p lowGPU -w i13hpc51 --gres=gpu:1 -o $outputFilename -e $outputFilename cmd_train_command_wrapper.sh $1 $2
-else
     nohup ./cmd_train_command_wrapper $1 $2 > $outputFilename &
+else
+    sbatch -t 3-00 -p lowGPU -w i13hpc51 --gres=gpu:1 -o $outputFilename -e $outputFilename cmd_train_command_wrapper.sh $1 $2
+
 fi
