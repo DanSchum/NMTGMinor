Bug fixes in Local Attention calculation. Added a lot of commentary.

onmt/modules/GlobalAttention.py
@@ -555,10 +555,10 @@ class LocalAttention(nn.Module):
 
         len_query, b = query.size(0), query.size(1) #batch_size_words x batch_size_sentence
         len_key, b_ = key.size(0), key.size(1)
+        len_prev_key = prev_k.size(1)
 
         key_mask = value_mask
 
-        # batch_size*h x len_query x d_head
         # project inputs to multi-heads
         proj_query = self.fc_query(query)  # batch_size x len_query x h*d_head
         shared_kv = group_linear([self.fc_key.function.linear, self.fc_value.function.linear], key)
@@ -578,11 +578,12 @@ class LocalAttention(nn.Module):
         v = v.contiguous().view(len_key, b * self.h, self.d_head).transpose(0, 1)
 
         #D.S: Local attention starts here
-        current_position = torch.cat((torch.zeros((1, step_num.item() * self.block_size, 1)).byte(),
-                       torch.ones((1, self.block_size, 1)).byte(),
-                       torch.zeros((1,
-                                    ((len_query - ((step_num.item() + 1) * self.block_size)) if
-                                     (len_query - ((step_num.item() + 1) * self.block_size)) >= 0 else 0), 1)).byte()), dim=1)
+        current_position = torch.cat(
+                        (torch.zeros((1, step_num.item() * self.block_size, 1)).byte(),
+                        torch.ones((1, self.block_size, 1)).byte(),
+                        torch.zeros((1,
+                                    ((len_prev_key - ((step_num.item() + 1) * self.block_size)) if
+                                     (len_prev_key - ((step_num.item() + 1) * self.block_size)) >= 0 else 0), 1)).byte()), dim=1)
         #Creates tensor (1 x len_query (Original Words Batch Size) x 1): With Dim 1 containing the index of the
         # current block to be set in the in the complete Words Batch Tensor
 
@@ -600,6 +601,9 @@ class LocalAttention(nn.Module):
             v = torch.cat([v, torch.zeros(v.shape[0], (prev_v.shape[1] - v.shape[1]), v.shape[2])], dim=1)
             #q = torch.cat([q, torch.zeros(q.shape[0], (prev_v.shape[1] - q.shape[1]), q.shape[2])], dim=1)
 
+        if k.shape[1] == 400:
+            print('Error found')
+
         k = torch.where(current_position, k, prev_k)
         #Use indizies from current_position to select from current k Tensor or previous k Tensor)
         v = torch.where(current_position, v, prev_v)
@@ -614,17 +618,18 @@ class LocalAttention(nn.Module):
         #After Transpose: k = ((b*h) x (Embedding/h) x (Batch_Size_Words))
         #attns = ((b*h) x (Batch_Size_Words) x Batch_Size_Words)
 
-        attns = attns.view(b, self.h, len_query, len_query)
-        #Attns = (b x h x Batch_Size_Words x Batch_Size_Words)
+        #attns = attns.view(b, self.h, len_query, len_query)  #Attns = (b x h x Batch_Size_Words x Batch_Size_Words)
+        attns = attns.view(b, self.h, len_query, len_prev_key)  #Attns = (b x h x Block_Size x Batch_Size_Words)
         mask_ = mask.unsqueeze(-3)
         #mask_ = (b x 1 x 1 x Batch_Size_Words)
         # FP16 support: cast to float and back
         attns = attns.float().masked_fill_(mask_, -float('inf')).type_as(attns)
+        #masked_fill fills -inf float values in all field of attention tensor, where mask = 1
         attns = F.softmax(attns.float(), dim=-1).type_as(attns)
         # return mean attention from all heads as coverage
         #coverage = torch.mean(attns, dim=1)
         attns = self.attn_dropout(attns)
-        attns = attns.view(b * self.h, len_query, len_query)
+        attns = attns.view(b * self.h, len_query, len_prev_key)
 
         # apply attns on value
         out = torch.bmm(attns, v)  # batch_size*h x len_query x d_head

onmt/modules/TransformerMemoryCompressed/Layers.py
@@ -331,10 +331,24 @@ class EncoderLayerLocalAttention(nn.Module):
         self.feedforward = Bottle(feedforward)
 
     def forward(self, query, split, attn_mask, step_num, prev_k, prev_v, pad_mask=None):
+        '''
+
+        :param query: Is the context value from Models implementation (Dim: Batch_Size_Words x Batch_Size_Sentence x Embedding_Size)
+        :param split: Contains the split values, which iterate through the src embedding (Dim: Block_Size x Batch_Size_Sentence x Embedding_Size)
+        :param attn_mask:
+        :param step_num:
+        :param prev_k:
+        :param prev_v:
+        :param pad_mask:
+        :return:
+        '''
         pad_mask = None
         query = self.preprocess_attn(query)
         split = self.preprocess_attn(split)
         out, k, v = self.multihead(query, split, split, attn_mask, step_num, prev_k, prev_v)
+        #Out: Contains Context (Dim: Batch_Size_Words x Batch_Size_Sentence x Embedding_Size)
+        #k & v: Contains k & v values including all last splits (Dim: heads x Batch_Size_Words x (Embedding_Size / heads))
+        #k & v: At Dim (1) - Batch_Size_Words: The Values are added from split, concatenated with the previous values from previous splits
         input = self.postprocess_attn(out, query)
 
         """ Feed forward layer 
@@ -444,7 +458,7 @@ class DecoderLayerLocalAttention(nn.Module):
         self.preprocess_ffn = PrePostProcessing(d_model, p, sequence='n')
         self.postprocess_ffn = PrePostProcessing(d_model, residual_p, sequence='da', static=onmt.Constants.static)
 
-        self.multihead_tgt = LocalAttention(h, d_model, block_size, self.cuda, attn_p=attn_p, static=onmt.Constants.static, share=1)
+        self.multihead_tgt = MultiHeadAttention(h, d_model, attn_p=attn_p, static=onmt.Constants.static, share=1)
         self.multihead_src = LocalAttention(h, d_model, block_size, self.cuda, attn_p=attn_p, static=onmt.Constants.static, share=2)
 
         if onmt.Constants.activation_layer == 'linear_relu_linear':
@@ -468,7 +482,7 @@ class DecoderLayerLocalAttention(nn.Module):
         self_context = query
 
         #D.S. Set to local attention layer
-        out, k, v = self.multihead_tgt(query, self_context, self_context, mask_tgt, step_num, prev_k, prev_v)
+        out, coverage = self.multihead_tgt(query, self_context, self_context, mask_tgt)
 
         if residual_dropout > 0:
             input_ = F.dropout(input, residual_dropout, self.training, False)

onmt/modules/TransformerMemoryCompressed/Models.py
@@ -147,7 +147,7 @@ class TransformerEncoderMemoryCompressed(nn.Module):
             print('Max Memory allocated (Before encoder forward): ' + str(torch.cuda.max_memory_allocated()))
             print('Real Memory allocated (Before encoder forward): ' + str(torch.cuda.memory_allocated()))
 
-        original_batch_size = context.shape[0]
+        #original_batch_size = context.shape[0]
         splits = torch.split(context, self.block_size, dim=0)
         for step_num, split in enumerate(splits):
             step_tensor = torch.tensor(step_num)
@@ -156,29 +156,21 @@ class TransformerEncoderMemoryCompressed(nn.Module):
                     #D.S: Handle Local Attention Layer
                     if len(self.layer_modules) - i <= onmt.Constants.checkpointing and self.training:
                         #def forward(self, query, key, value, mask, step_num, prev_k, prev_v, query_mask=None, value_mask=None):
-                        #TODO: Check why checkpoint is not done even its activated
+                        #Context is used as v-Values, which are mulitplied with the attention (attn = q * k)
                         context, prev_k, prev_v = checkpoint(custom_layer(layer), context, split, mask_src,
                                                              step_tensor, states_k, states_v)
+                        #Return values:
+                        #Context contains
+                        #prev_k & prev_v contains k anv v values from all previous splits, which are needed for the
+                        # next split in where(...) operation, to concat the previous k & v values with the current one (From current split)
                     else:
                         context, prev_k, prev_v = layer(context, split, mask_src,
                                                         step_tensor, states_k, states_v)  # batch_size x len_src x d_model
-
-
-                elif type(layer) is EncoderLayerMemoryCompressed:
-                    #D.S: Handle Memory Compressed Layer
-                    if len(self.layer_modules) - i <= onmt.Constants.checkpointing and self.training:
-                        # def forward(self, query, key, value, mask, query_mask=None, value_mask=None):
-                        context, prev_k, prev_v = checkpoint(custom_layer(layer), context, mask_src)
-
-                    else:
-                        context, prev_k, prev_v = layer(context, mask_src)  # batch_size x len_src x d_model
-
                 else:
                     raise NotImplementedError
 
-            states_k = prev_k
-            states_v = prev_v
-
+                states_k = prev_k #Update states with k & v values from previous splits
+                states_v = prev_v
 
         # From Google T2T
         # if normalization is done in layer_preprocess, then it should also be done
@@ -400,8 +392,8 @@ class TransformerDecoderMemoryCompressed(nn.Module):
             emb = emb[0]
         emb = self.preprocess_layer(emb)
 
-        #mask_src = src.eq(onmt.Constants.PAD).unsqueeze(1)
-        mask_src = torch.split(src, self.block_size, dim=-1)[0].eq(onmt.Constants.PAD).unsqueeze(1)
+        mask_src = src.eq(onmt.Constants.PAD).unsqueeze(1)
+        #mask_src = torch.split(src, self.block_size, dim=-1)[0].eq(onmt.Constants.PAD).unsqueeze(1)
 
         pad_mask_src = src.data.ne(onmt.Constants.PAD)
 
@@ -413,14 +405,15 @@ class TransformerDecoderMemoryCompressed(nn.Module):
         # D.S: Context is splitted in seperate parts
         batch_sentences = output.shape[1]
 
-        states_k = torch.zeros((self.n_heads * batch_sentences, output.shape[0], (self.model_size // self.n_heads)))
-        states_v = torch.zeros((self.n_heads * batch_sentences, output.shape[0], (self.model_size // self.n_heads)))
+        states_k = torch.zeros((self.n_heads * batch_sentences, context.shape[0], (self.model_size // self.n_heads)))
+        states_v = torch.zeros((self.n_heads * batch_sentences, context.shape[0], (self.model_size // self.n_heads)))
 
         if self.cuda:
             states_k = states_k.cuda()
             states_v = states_v.cuda()
 
-        splits = torch.split(output, self.block_size, dim=0)
+        #TODO: D.S. Is the split on output correct? Should here the split on context/input be done?
+        splits = torch.split(context, self.block_size, dim=0)
         for step_num, split in enumerate(splits):
             step_tensor = torch.tensor(step_num)
             for i, layer in enumerate(self.layer_modules):
@@ -429,29 +422,19 @@ class TransformerDecoderMemoryCompressed(nn.Module):
                     # D.S: Handle Local Attention Layer
                     if len(self.layer_modules) - i <= onmt.Constants.checkpointing and self.training:
                         # def forward(self, query, key, value, mask, step_num, prev_k, prev_v, query_mask=None, value_mask=None):
-                        # TODO: Check why checkpoint is not done even its activated
+                        #output, prev_k, prev_v = checkpoint(custom_layer(layer), output,
+                        #                                     split, mask_tgt, mask_src, step_tensor, states_k, states_v)
                         output, prev_k, prev_v = checkpoint(custom_layer(layer), output,
-                                                             split, mask_tgt, mask_src, step_tensor, states_k, states_v)
+                                                            split, mask_tgt, mask_src, step_tensor, states_k, states_v)
                     else:
                         output, prev_k, prev_v = layer(output, split, mask_tgt, mask_src, step_tensor,
                                                         states_k, states_v)  # batch_size x len_src x d_model
-
-
-                elif type(layer) is EncoderLayerMemoryCompressed:
-                    # D.S: Handle Memory Compressed Layer
-                    if len(self.layer_modules) - i <= onmt.Constants.checkpointing and self.training:
-                        # def forward(self, query, key, value, mask, query_mask=None, value_mask=None):
-                        output, prev_k, prev_v = checkpoint(custom_layer(layer), output, mask_src)
-
-                    else:
-                        output, prev_k, prev_v = layer(output, mask_src)  # batch_size x len_src x d_model
-
                 else:
                     raise NotImplementedError
 
-            #Write states from last split to input of next split
-            states_k = prev_k
-            states_v = prev_v
+                #Write states from last split to input of next split
+                states_k = prev_k
+                states_v = prev_v
 
         # From Google T2T
         # if normalization is done in layer_preprocess, then it should also be done